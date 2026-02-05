edf4d63b414cc99fc2813c3cb07597fa74ee56e5
Tean Lee <leetean2@gmail.com>
2026-02-05 12:14:19 +1100

    Add API service layer for standings and race data

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_BASE = 'http://localhost:8080/api/v1';
+
+export const getDriverStandings = () =>
+    axios.get(`${API_BASE}/championship/drivers`);
+
+export const getConstructorStandings = () =>
+    axios.get(`${API_BASE}/championship/constructors`)
+
+export const getNextRace = () =>
+    axios.get(`${API_BASE}/races/next`)
+
+export const getLastRace = () =>
+    axios.get(`${API_BASE}/races/last`)
+
+export const getPrediction = () => 
+  axios.get(`${API_BASE}/predictions/next-race`);
